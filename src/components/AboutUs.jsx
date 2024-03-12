e7f340435cf64680ec7595944cbca8c97b92242f
Ezaz Ahmed <ezazrahul794@gmail.com>
2024-03-13 00:16:00 +0600

    'test'

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -10,10 +10,12 @@ const AboutUs = () => {
           <p className='text-justify text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur earum molestias voluptate soluta aliquid error possimus eum asperiores? Ex reprehenderit reiciendis nam necessitatibus aut assumenda, aliquid consequuntur ab minima, possimus excepturi facilis ipsa! Velit earum enim eaque, accusantium qui cum.</p>
         </div>
         <div className="">
-          <img
-            className='rounded-lg shadow-md h-full w-full mx-0 justify-center block'
-            src='https://cdn.pixabay.com/photo/2018/02/27/21/24/thread-3186657_960_720.jpg'
-            alt='bonds-bd-aboutus' />
+          <div className="">
+            <img
+              className='rounded-lg shadow-md h-full w-full mx-0 justify-center block'
+              src='https://cdn.pixabay.com/photo/2018/02/27/21/24/thread-3186657_960_720.jpg'
+              alt='bonds-bd-aboutus' />
+          </div>
         </div>
       </div>
     </div>
