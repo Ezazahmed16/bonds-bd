5837156ee75a246d3554e9ae783cc5570dd9a6d5
Ezaz Ahmed <ezazrahul794@gmail.com>
2024-03-13 20:30:56 +0600

    'fix-error'

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,17 @@ import Mission from "@/components/Mission";
 import Products from "@/components/Products";
 import Teams from "@/components/Teams";
 import Vision from "@/components/Vision";
-export default function Home() {  
+export default function Home() {
   return (
     <div className="">
       <Banner />
       <AboutUs />
       <div style={{ backgroundColor: '#1D232A' }} className="mx-auto max-w-7xl justify-center items-center pb-20">
-      <div className="flex flex-col w-full lg:flex-row gap-5">
-        <Mission />
-        <div className="divider lg:divider-horizontal"></div>
-        <Vision />
-      </div>
+        <div className="flex flex-col w-full lg:flex-row gap-5">
+          <Mission />
+          <div className="divider lg:divider-horizontal"></div>
+          <Vision />
+        </div>
       </div>
       <Products />
       <Teams />
